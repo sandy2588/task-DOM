6c4767dcfa0c14d3a4b585665f3657de3badaa28
sandy2588 <sandyhisham258@gmail.com>
2024-09-02 21:55:37 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,15 @@
+document.querySelector("#Red").onclick = function(){
+
+    document.querySelector("#result").style.color ="Red";
+}
+
+ document.querySelector("#blue").onclick = function(){
+
+    document.querySelector("#result").style.color ="blue";
+ }
+
+
+document.querySelector("#Green").onclick = function(){
+
+document.querySelector("#result").style.color ="Green";
+}
